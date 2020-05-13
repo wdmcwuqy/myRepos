2cc3477768d9e57c7217b3ffe89eae86800087bb
wdmcwuqy <30914516+wdmcwuqy@users.noreply.github.com>
2020-05-13 16:27:19 +0800

    Update system-addon.js

--- system-addon.js
+++ system-addon.js
@@ -1,28 +1,29 @@
 let addon = {
   preFlight: function (context) {
     console.log('preFlight', context)
+    return true
   },
   nodes: [],
   pods: [],
   containers: []
 }
 
-function openPodMonitor(context){
+function openPodMonitor1(context){
 	console.log('点击调整pms系统', context)
 	window.open("www.baidu.com", '_blank')
 }
 
-let podMonitor = {
+let podMonitor1 = {
   title: "跳转融媒体管理系统",
   icon: 'el-skip-cm-sites-pms',
   loading: false,
   visible: function (context) {
-    // console.log('检查节点监控是否可见 pod', context)
+    console.log('检查节点监控是否可见 pod pms------------', context)
     return true
   },
-  openMonitoringPage: openPodMonitor
+  openMonitoringPage: openPodMonitor1
 }
 
-addon.pods.push(podMonitor)
+addon.nodes.push(podMonitor1)
 
 window.EIP_MONITOR_ADDON_TO_ACTIVATE = addon
